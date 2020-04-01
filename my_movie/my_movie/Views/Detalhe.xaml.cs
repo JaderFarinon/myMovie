daa0f072da86c4aff9783bc4a10e72b0bfc98346
JaderFarinon <jadergfarinon@gmail.com>
2020-04-01 16:31:32 -0300

    Comentários
    Código comentado

--- my_movie/my_movie/Views/Detalhe.xaml.cs
+++ my_movie/my_movie/Views/Detalhe.xaml.cs
@@ -13,15 +13,23 @@ namespace my_movie.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Detalhe : ContentPage
     {
+        //Recebe o objeto tipo Filme
         public Detalhe(Filme selFilme)
         {
             InitializeComponent();
+            //Título do filme na barra
             page.Title = selFilme.title;
+            //Imagem do poster
             imgposter.Source = selFilme.poster_path;
+            //Resumo do filme
             lbloverview.Text = selFilme.overview;
+            //Barra indicando avaliação
             imgbarra.WidthRequest = selFilme.bar_widht;
+            //Data de lançamento
             lblrelease.Text = "Lançamento: " + DateTime.Parse(selFilme.release_date).ToString("dd/MM/yyyy");
+            //Gênero
             lblgenero.Text = "Gênero: " + selFilme.genre_ids;
+            //Imagem maior backdrop
             imgbg.Source = selFilme.backdrop_path;
         }
     }
--- my_movie/my_movie/Views/Lista.xaml.cs
+++ my_movie/my_movie/Views/Lista.xaml.cs
@@ -9,15 +9,21 @@ namespace my_movie.Views
     {
         public Lista()
         {
+            //Desabilita barra de navegação na tela da lista
             NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
+            //Deserializa o json e passa como fonte da lista
             moviesList.ItemsSource = Deserialize.Deserializar(Request.jsonReq());
+            //Ao selecionar item da lista
             moviesList.ItemSelected += ListaFilmes_ItemSelected;
         }
 
+        //Ao selecionar item da lista
         private async void ListaFilmes_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
+            //obtem dados so filme selecionado
             var selFilme = e.SelectedItem as Filme;
+            //Chama a view de detalhamento do filme passando o objeto de tipo Filme
             await Navigation.PushAsync(new Detalhe(selFilme));
         }
     }
