17fd999d36a2a407edb489fce9b194deb41207d4
Victor <48594521+bochilof@users.noreply.github.com>
2020-04-29 18:57:58 -0300

    Create RootObject.cs

---
+++ my_movie/my_movie/Classes/RootObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace my_movie.Classes
+{
+    public class RootObject
+    {
+        public int Page { get; set; }
+        public int Total_results { get; set; }
+        public int Total_pages { get; set; }
+        public List<Filme> Results { get; set; }
+    }
+}
